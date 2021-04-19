326aaffd6399eb871fd24bac9821d564d4530ea6
James Bullen <jambun@gmail.com>
2021-04-19 10:13:18 +1000

    don't use js.erb because it may not work on a dist

--- frontend/assets/note_defaults.js
+++ frontend/assets/note_defaults.js
@@ -1,8 +1,6 @@
-<% if AppConfig.has_key?(:note_defaults) %>
+$(function() {
 
-  var note_defaults = <%= ASUtils.to_json(AppConfig[:note_defaults]) %>;
-
-  $(function() {
+  if (typeof NOTE_DEFAULTS !== 'undefined') {
 
     var set_note_default = function(select) {
       var ntype = $(select).val();
@@ -10,9 +8,9 @@
       var ta = $(select).closest('div.subrecord-form-container').find('textarea')[0];
       var cm = $(ta).data('CodeMirror');
 
-      if (!cm.getValue() || Object.values(note_defaults).includes(cm.getValue())) {
-        if (note_defaults[ntype]) {
-          cm.setValue(note_defaults[ntype]);
+      if (!cm.getValue() || Object.values(NOTE_DEFAULTS).includes(cm.getValue())) {
+        if (NOTE_DEFAULTS[ntype]) {
+          cm.setValue(NOTE_DEFAULTS[ntype]);
         } else {
           cm.setValue('');
         }
@@ -27,6 +25,7 @@
       set_note_default($(this).closest('div.subrecord-form-fields').find('select.note-type')[0]);
     });
 
-  });
+  }
 
-<% end %>
+});
+
